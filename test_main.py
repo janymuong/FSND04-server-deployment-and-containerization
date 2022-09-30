9423fe27639edf2f51f973aacf4219d498102702
Jany Muong <me.roumuong@gmail.com>
2022-09-30 13:14:11 +0300

    read secrets from .env_file

--- test_main.py
+++ test_main.py
@@ -7,10 +7,10 @@ import pytest
 
 import main
 
-SECRET = 'TestSecret'
-TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-EMAIL = 'wolf@thedoor.com'
-PASSWORD = 'huff-puff'
+SECRET = os.getenv('SECRET')
+TOKEN = os.getenv('TOKEN')
+EMAIL = os.getenv('EMAIL')
+PASSWORD = os.getenv('PASSWORD')
 
 @pytest.fixture
 def client():
